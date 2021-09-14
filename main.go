2724f2e2562f81f24dd786c7757523da8e4ae449
Nico Toledo <odelotsalocin@gmail.com>
2021-09-14 17:13:08 -0300

    funcionando para cadena ejemplo NN040001. Retorna error para cadenas mas cortas

--- main.go
+++ main.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	p := model.NewResult("Nico", "Sape", 10)
 	fmt.Println(p)
+	fmt.Println(model.GetStructure("Nico"))
+	fmt.Println("----------------")
+	fmt.Print(model.GetStructure("NN040001"))
 }
--- model/entities.go
+++ model/entities.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"strconv"
+)
+
 type Result struct {
 	Type   string
 	Value  string
@@ -8,4 +12,17 @@ type Result struct {
 
 func NewResult(Type string, Value string, Length int) Result {
 	return Result{Type, Value, Length}
+}
+
+func GetStructure(chain string) Result {
+	Type := chain[0:2]
+	LengthStr := chain[2:4]
+	Value := chain[4:]
+	LengthInt, err := strconv.Atoi(LengthStr) //convierte string en int
+	if err == nil {
+		return NewResult(Type, Value, LengthInt)
+	} else {
+		return NewResult("Error", "Error", 00)
+	}
+
 }
