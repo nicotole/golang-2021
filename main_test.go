5103659b9043a6e27f97d595f249d63cb461e394
Nico Toledo <odelotsalocin@gmail.com>
2021-09-14 19:57:04 -0300

    go test main_test.go .count=1 (resulto Ok)

--- main_test.go
+++ main_test.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"entregableGo/model"
+	"fmt"
 	"testing"
 )
 
 func TestInputChains(t *testing.T) {
-
+	chainTest := "TX040001"
+	result := model.GetStructure(chainTest)
+	st := model.NewResult("TX", 04, "0001")
+	if result != st {
+		fmt.Println("No funciona")
+	}
 }
