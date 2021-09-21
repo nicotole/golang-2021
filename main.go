f37df2ebfd1f9ad132bf80581f8c72b752f17e12
Nico Toledo <odelotsalocin@gmail.com>
2021-09-21 16:27:43 -0300

    coverage 0.0%

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 )
 
 func main() {
-	p := model.NewResult("Nico", 10, "Sape")
-	fmt.Println(p)
 	fmt.Println(model.GetStructure("Nico"))
 	fmt.Println("----------------")
 	fmt.Print(model.GetStructure("NN040001"))
--- main_test.go
+++ main_test.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"entregableGo/model"
-	"fmt"
 	"testing"
 )
 
 func TestInputChains(t *testing.T) {
 	//chainTest := "TX040001"
-	chainTest := "TB020"
-	result := model.GetStructure(chainTest)
+	/*chainTest := "TB020"
+	result, _ := model.GetStructure(chainTest)
 	st := model.NewResult("TX", 04, "0001")
 	if result != st {
 		fmt.Println("No funciona")
+	} */
+
+	chainTest := "TX040001"
+	//chainTest := "TB020"
+	result, err := model.GetStructure(chainTest)
+
+	if result == nil {
+		t.Error(err)
+	} else {
+		t.Log(result)
 	}
 
 }
